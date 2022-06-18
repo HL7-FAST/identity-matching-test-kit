f1534e4ba35c7cbffdbe9f06586e0599bbc1ea23
napolr1 <92889022+napolr1@users.noreply.github.com>
2022-06-18 11:49:55 -0400

    Update match_operation.rb

--- lib/identity_matching/match_operation.rb
+++ lib/identity_matching/match_operation.rb
@@ -1,41 +1,93 @@
-require_relative 'inferno_template/patient_group'
+require "uri"
+require "json"
+require "net/http"
+require 'webmock/rspec'
+require 'logger'
 
 module InfernoTemplate
-  class Suite < Inferno::TestSuite
-    id :test_suite_template
-    title 'Inferno Test Suite Template'
-    description 'A basic test suite template for Inferno'
+ 
+  class PatientGroup < Inferno::TestGroup
+    title 'Patient  Tests'
+    description 'Verify that the server makes Patient resources available'
+    id :patient_group
 
-    # This input will be available to all tests in this suite
-    input :url
+        
+    test do
+      title 'Patient match is valid'
+      description %(
+        Verify that the Patient  $match resource returned from the server is a valid FHIR resource.
+      )
+      
+      input :search_json ,
+        type: 'textarea'
+      
+      output :response_json
+      # Named requests can be used by other tests
+      makes_request :match
+      
+      
 
-    # All FHIR requests in this suite will use this FHIR client
-    fhir_client do
-      url :url
-    end
+      logger= Logger.new(STDOUT)
+      # create a named client for a group
+      fhir_client  do
+        url :url
+      end
+ 
+      run do
+        body = JSON[search_json] 
+        
 
-    # Tests and TestGroups can be defined inline
-    group do
-      id :capability_statement
-      title 'Capability Statement'
-      description 'Verify that the server has a CapabilityStatement'
+          #puts("request url=" +  url)
+          #puts("request body= #{body}" )
+          fhir_operation("Patient/$match", body: body, client: :default, name: nil, headers: { 'Content-Type': 'application/fhir+json' })
 
-      test do
-        id :capability_statement_read
-        title 'Read CapabilityStatement'
-        description 'Read CapabilityStatement from /metadata endpoint'
+          #puts("response body=" +  response[:body])
+          #fhir_operation("Patient/$match", body: body, client: :default, name: nil, headers: { 'Content-Type': 'application/fhir+json' })
 
-        run do
-          fhir_get_capability_statement
-
+          
+          #response_json=response[:body]
+          output response_json: response[:body]
+          #output response_json: response_json
           assert_response_status(200)
-          assert_resource_type(:capability_statement)
-        end
+          #assert_resource_type(:bundle)
+             
       end
     end
-
-    # Tests and TestGroups can be written in separate files and then included
-    # using their id
-    group from: :patient_group
+    test do
+      input :expectedResultCnt
+      input :response_json
+      output :numberOfRecordsReturned
+      title 'Patient match - determines whether or not the $match function returns every valid record'
+      description %(Match output SHOULD contain every record of every candidate identity, subject to volume limits
+      )
+      run do
+        
+          #puts response_json  
+          response = JSON[response_json]
+          assert_valid_json(response_json, message = "Invalid JSON response received - expected: #{expectedResultCnt} - received: #{numberOfRecordsReturned}")
+          numberOfRecordsReturned = response['total'] 
+          puts "number of records returned in bundle ---- #{numberOfRecordsReturned} " 
+          puts "number of records expected in bundle ---- #{expectedResultCnt} " 
+          assert numberOfRecordsReturned.to_s() == expectedResultCnt.to_s(), "Incorrect Number of Records returned"
+          
+          output numberOfRecordsReturned: numberOfRecordsReturned
+            
+         
+      end
+      run do
+        
+        #puts response_json  
+        response = JSON[response_json]
+        assert_valid_json(response_json, message = "Invalid JSON response received - expected: #{expectedResultCnt} - received: #{numberOfRecordsReturned}")
+        numberOfRecordsReturned = response['total'] 
+        puts "number of records returned in bundle ---- #{numberOfRecordsReturned} " 
+        puts "number of records expected in bundle ---- #{expectedResultCnt} " 
+        assert numberOfRecordsReturned.to_s() == expectedResultCnt.to_s(), "Incorrect Number of Records returned"
+        
+        output numberOfRecordsReturned: numberOfRecordsReturned
+          
+       
+    end
+    end
   end
 end
