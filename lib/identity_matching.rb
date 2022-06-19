8a2ad2e02d7b27c134c5a280c215b14fec06b0a6
napolr1 <92889022+napolr1@users.noreply.github.com>
2022-06-19 11:42:58 -0400

    Update identity_matching.rb

--- lib/identity_matching.rb
+++ lib/identity_matching.rb
@@ -1,34 +1,22 @@
-require 'inferno/dsl/oauth_credentials'
-require_relative 'identity_matching/match_operation'
-#require_relative 'identity_matching/digital_identity'
-#require_relative 'identity_matching/identity_assurance'
-#require_relative 'identity_matching/patient_matching'
 
-require_relative 'identity_matching/match_operation'
 
-module IdentityMatching
+require_relative 'inferno_template/patient_group'
+
+module InfernoTemplate
   class Suite < Inferno::TestSuite
-    id :identity_matching
-    title 'Identity Matching'
-    description 'Test suite for Identity Matching'
+    id :test_suite_template
+    title 'Inferno Test Suite Template'
+    description 'A basic test suite template for Inferno'
 
     # This input will be available to all tests in this suite
-    input :url,
-        title: 'FHIR endpoint',
-        description: 'URL of FHIR endpoint'
-    #begin
-    input :smart_credentials,
-        title: 'OAuth credentials',
-        type: :oauth_credentials,
-        optional: true
-    #end
+    input :url
 
     # All FHIR requests in this suite will use this FHIR client
     fhir_client do
       url :url
-      #, oauth_credentials :smart_credentials ***ENABLE WHEN SERVER READY ****
     end
 
+    # Tests and TestGroups can be defined inline
     group do
       id :capability_statement
       title 'Capability Statement'
@@ -40,22 +28,16 @@ module IdentityMatching
         description 'Read CapabilityStatement from /metadata endpoint'
 
         run do
-          fhir_client.set_no_auth
           fhir_get_capability_statement
 
           assert_response_status(200)
           assert_resource_type(:capability_statement)
-          assert_valid_resource
-
         end
       end
-    end    
+    end
 
-    # Specify identity match test groups
-    
-    group from: :match_operation
-    #group from: :digital_identity
-    #group from: :identity_assurance
-    #group from: :patient_matching
+    # Tests and TestGroups can be written in separate files and then included
+    # using their id
+    group from: :patient_group
   end
 end
