bdb329eb9920a7b68e028cddf64df19b64adf5d5
Shaumik-Ashraf <shaumikashraf@mitre.org>
2022-08-16 10:58:18 -0400

    debug Technical tests group

--- lib/identity_matching/technical.rb
+++ lib/identity_matching/technical.rb
@@ -13,6 +13,10 @@ module IdentityMatching
     DESC
     id :technical
 
+    http_client do
+        url :url # from test suite input
+    end
+
     test do
       title 'Server can create patients'
       id :create
@@ -22,13 +26,9 @@ module IdentityMatching
         patient_json = load_resource('technical_create_patient.json')
         patient_fhir = FHIR.from_contents(patient_json)
 
-        begin
-            @new_patient_id = client.create(patient_fhir).id
-        rescue Exception => e
-            fail "Error #{e.class}"
-        else
-            pass
-        end
+        response = post('/Patient', body: patient_fhir.to_json, headers: {'Content-Type' => 'application/fhir+json'})
+        assert (response.status > 199 && response.status < 300) or (response.status == 303)
+        pass
       end
     end
 
@@ -38,31 +38,18 @@ module IdentityMatching
       description "Expects route GET <base>/Patient"
 
       run do
-        begin
-            patients = fhir_read 'Patient'
-            patients_fhir = FHIR.from_contents(patients)
+        response = get('Patient', headrs: {'Content-Type' => 'application/fhir+json'})
+        patients_fhir = FHIR.from_contents(response.response_body)
+        assert patients_fhir, "Could not parse FHIR response"
+        assert response.status == 200, "Expected HTTP 200 OK"
 
-            # check response fhir syntax
-            @errors = patients_fhir.validate
-            fail(errors.to_s) unless @errors.empty?
+        # check response fhir syntax
+        @errors = patients_fhir.validate
+        fail(errors.to_s) unless @errors.empty?
 
-            # check response is FHIR bundle with atleast 1 Patient
-            fail("Expected FHIR Bundle in response") if patients.resourceType != 'Bundle'
-            fail("Expected patient entries in bundle") if patients.total < 1
-
-            # check bundle entries for created patient by id
-            found = false
-            patients.entry.each do |ent|
-                id = ent.to_hash.dig('resource', 'id')
-                type = ent.to_hash.dig('resource', 'resourceType')
-                found ||= ((id == @new_patient_id) && (type == 'Patient'))
-            end
-            fail("Could not find newly created patient") if !found
-        rescue Exception => e
-            fail "Error: ..."
-        else
-            pass
-        end
+        # check response is FHIR bundle with atleast 1 Patient
+        fail("Expected FHIR Bundle in response") if patients_fhir.resourceType != 'Bundle'
+        fail("Expected patient entries in bundle") if patients_fhir.total < 1
       end
     end
 
